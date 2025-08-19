5eb8c2073c1a9845dbbd9071b94f7ed520f88f60
muzzik <1226085293@qq.com>
2025-08-19 22:02:59 +0800

    更新内容

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -38,7 +38,7 @@ export function Navbar() {
 					<div className="flex items-center justify-between sm:gap-2">
 						<div className="flex ml-4 sm:ml-0">
 							<Link
-								href="https://github.com/nisabmohd/NexDocs"
+								href="https://github.com/1226085293/MKFrameworkDocs"
 								className={buttonVariants({
 									variant: "ghost",
 									size: "icon",
