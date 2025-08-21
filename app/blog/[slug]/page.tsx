96b90e150c4ecf5f22080fa49093387db3c01388
muzzik <1226085293@qq.com>
2025-08-21 23:19:58 +0800

    删除多余文件

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -4,7 +4,11 @@ import { buttonVariants } from '@/components/ui/button';
 import { ArrowLeftIcon } from 'lucide-react';
 import Link from 'next/link';
 import { notFound } from 'next/navigation';
-import BlogPageWrapper from '@/components/blog-page-wrapper'; // ✅
+import { Authors } from '@/components/authors';
+import MdxRenderer from '@/components/mdx-renderer';
+import { Typography } from '@/components/typography';
+import { formatDate } from '@/lib/utils';
+import Image from 'next/image';
 
 type PageProps = {
     params: Promise<{ slug: string }>;
@@ -44,8 +48,32 @@ export default async function BlogPage(props: PageProps) {
                 <ArrowLeftIcon className="w-4 h-4 mr-1.5" /> Back to blog
             </Link>
 
-            {/* 将 blog 数据交给专门的客户端组件去渲染 */}
-            <BlogPageWrapper blog={blog} />
+            <div className="flex flex-col gap-3 pb-7 w-full mb-2">
+                <p className="text-muted-foreground text-sm">{formatDate(blog.date)}</p>
+                <h1 className="sm:text-3xl text-2xl font-extrabold">{blog.title}</h1>
+                <div className="mt-6 flex flex-col gap-3">
+                    <p className="text-sm text-muted-foreground">Posted by</p>
+                    <Authors authors={blog.authors} />
+                </div>
+            </div>
+            {/* <div className="mt-6 flex flex-col gap-3">
+                            <p className="text-sm text-muted-foreground">Posted by</p>
+                            <Authors authors={blog.authors} />
+                        </div> */}
+            <div className="!w-full">
+                <div className="w-full mb-7">
+                    <Image
+                        src={blog.cover}
+                        alt="cover"
+                        width={700}
+                        height={400}
+                        className="w-full h-[400px] rounded-md border object-cover"
+                    />
+                </div>
+                <Typography>
+                    <MdxRenderer code={blog.body.code} />
+                </Typography>
+            </div>
         </div>
     );
 }
