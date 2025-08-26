abe6c2fd3adc5b44eb050e818380e6a8c610d713
yuanfengxin <1226085293@qq.com>
2025-08-26 13:41:05 +0800

    更新文档

--- components/markdown/image.tsx
+++ components/markdown/image.tsx
@@ -7,13 +7,15 @@ type Width = ComponentProps<typeof NextImage>['width'];
 export default function Image({
     src,
     alt = 'alt',
-    width = 800,
-    height = 350,
+    width = 0,
+    height = 0,
     ...props
 }: ComponentProps<'img'>) {
     if (!src) return null;
+
     return (
         <NextImage
+            className="w-auto h-auto"
             src={src as string}
             alt={alt}
             width={width as Width}
